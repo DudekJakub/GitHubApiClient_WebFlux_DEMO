c99de854978a4ddab8285b5f7724b765cd768258
jDudek <jakub.dudek94@gmail.com>
2023-06-06 21:31:03 +0200

    main run class added

---
+++ src/main/java/com/demo/githubapiclient/GitHubApiClientWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.demo.githubapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubApiClientWebClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubApiClientWebClientApplication.class, args);
+    }
+
+}
