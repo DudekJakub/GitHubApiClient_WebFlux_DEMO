069d83bc745a02ca3135e716f8bdfb6fd61d3150
jDudek <jakub.dudek94@gmail.com>
2023-06-06 21:33:04 +0200

    main test class added

---
+++ src/test/java/com/demo/githubapiclient/GitHubApiClientWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.githubapiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubApiClientWebClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
